6b90162d39cbba4557880465c3ea82ace9d49287
Tahaldr <tahanaitdo@gmail.com>
2025-05-22 06:29:14 +0100

    Added vim-blade & noice

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -13,6 +13,27 @@ return {
     end,
   },
 
+  -- Vim syntax highlighting for Blade templates (Laravel 4+).
+  {
+    "jwalton512/vim-blade",
+    ft = "blade",
+  },
+
+  -- Highly experimental plugin that completely replaces the UI for messages, cmdline and the popupmenu.
+  {
+    "folke/noice.nvim",
+    config = require "configs.noice",
+    event = "VeryLazy",
+    cmdline = {
+      view = "cmdline_popup",
+    },
+
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+  },
+
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
 
