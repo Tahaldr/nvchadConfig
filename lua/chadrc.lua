3889e4c0af5652b7ab402bcbf5bfc7789a93f841
Tahaldr <tahanaitdo@gmail.com>
2025-05-22 05:54:22 +0100

    Changed some UI configs

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,24 +1,70 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
----@type ChadrcConfig
+-- ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-	theme = "onedark",
+  theme = "onedark",
 
-	hl_override = {
-		Comment = { italic = true },
-		["@comment"] = { italic = true },
-	},
+  theme_toggle = { "onedark", "one_light" },
+
+  hl_override = {
+    Comment = { italic = true },
+    ["@comment"] = { italic = true },
+  },
 }
 
--- M.nvdash = { load_on_startup = true }
--- M.ui = {
---       tabufline = {
---          lazyload = false
---      }
---}
+M.nvdash = {
+  load_on_startup = true,
+
+  header = {
+    " ██▓    ▄▄▄      ▒███████▒ ██▓",
+    "▓██▒   ▒████▄    ▒ ▒ ▒ ▄▀░▓██▒",
+    "▒██░   ▒██  ▀█▄  ░ ▒ ▄▀▒░ ▒██▒",
+    "▒██░   ░██▄▄▄▄██   ▄▀▒   ░░██░",
+    "░██████▒▓█   ▓██▒▒███████▒░██░",
+    "░ ▒░▓  ░▒▒   ▓▒█░░▒▒ ▓░▒░▒░▓  ",
+    "░ ░ ▒  ░ ▒   ▒▒ ░░░▒ ▒ ░ ▒ ▒ ░",
+    "  ░ ░    ░   ▒   ░ ░ ░ ░ ░ ▒ ░",
+    "    ░  ░     ░  ░  ░ ░     ░  ",
+    "                 ░            ",
+    "                              ",
+    "                              ",
+    "                              ",
+  },
+
+  buttons = {
+    { txt = "  Find File", keys = "ff", cmd = "Telescope find_files" },
+    { txt = "  Recent Files", keys = "fo", cmd = "Telescope oldfiles" },
+    { txt = "󰈭  Find Word", keys = "fw", cmd = "Telescope live_grep" },
+    { txt = "󱥚  Themes", keys = "th", cmd = ":lua require('nvchad.themes').open()" },
+    { txt = "  Mappings", keys = "ch", cmd = "NvCheatsheet" },
+  },
+}
+M.ui = {
+  tabufline = {
+    enabled = true,
+    lazyload = true,
+    order = { "treeOffset", "buffers", "tabs", "btns" },
+    modules = nil,
+    bufwidth = 21,
+  },
+
+  statusline = {
+    theme = "vscode_colored",
+    separator_style = "arrow",
+  },
+
+  telescope = { style = "bordered" },
+
+  colorify = {
+    enabled = true,
+    mode = "virtual", -- fg, bg, virtual
+    virt_text = "󱓻 ",
+    highlight = { hex = true, lspvars = true },
+  },
+}
 
 return M
