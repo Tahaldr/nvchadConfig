752d20af96bcecf0967893a47d7d22307c0346fd
Tahaldr <tahanaitdo@gmail.com>
2025-05-23 21:17:59 +0100

    Added terminal mapping shortCut

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,7 +5,13 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 -- map("n", ";", ":", { desc = "CMD enter command mode" })
+
+-- for switching to Normal Mode
 map("i", "jk", "<ESC>")
 map("i", "jj", "<ESC>")
+
+-- for exiting terminal
+map('t', '<ESC>', [[<C-\><C-n>]], { noremap = true, silent = true })
+
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
